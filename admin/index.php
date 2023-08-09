b9e2bce413ce740de024249b6d6778697705a326
aakash193 <89975560+aakash193@users.noreply.github.com>
2023-08-09 16:14:04 +0545

    Coursework
    Coursework

---
+++ admin/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Redirect to index.php</title>
+</head>
+<body>
+  <script>
+    // JavaScript code to redirect to ../index.php
+    window.location.href = '../index.php';
+  </script>
+</body>
+</html>
